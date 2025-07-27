5f95645cec6b42a09a5447192e0845ac33106007
Dimitrios Valodimos <dimitriosvalodimos@gmail.com>
2025-07-27 12:11:54 +0200

    update price list and products

--- src/text.tsx
+++ src/text.tsx
@@ -37,107 +37,105 @@ export const products: TProductInfo[] = [
         description:
             "Luxus Pur - Regeneration in höchster Qualität, in Verbindung mit einer Edelstein-Massage entsteht ein außergewöhnliches Erlebnis",
         priceList: [
-            { time: 90, price: 159 },
-            { time: 60, price: 119 },
+            { time: 90, price: 169 },
+            { time: 60, price: 129 },
         ],
-    },
-    {
-        title: "Clarins - Rendezvous der Pflanzen",
-        description:
-            "Hochwirksame Pflegeextrakte berühren einzigartig Ihre Sinne: Jünger - vitaler - schöner",
-        priceList: [{ time: 90, price: 139 }],
     },
     {
         title: "Therapie Balance",
         description:
             "Japanische Energiemassage, berührt Ihre Seele - eine entspannende Gesichtsbehandlung",
-        priceList: [{ time: 70, price: 109 }],
+        priceList: [{ time: 70, price: 119 }],
     },
     {
         title: "Comfort-Anti Aging",
         description:
             "Wirkstoffbehandlung, die nachhaltig den Regenerations-Prozess der Haut unterstützt",
-        priceList: [{ time: 60, price: 99 }],
+        priceList: [
+            { time: 90, price: 149 },
+            { time: 60, price: 109 },
+        ],
     },
     {
         title: "Feuchtigkeit- Hyaluron",
         description:
             "Intensive Durchfeuchtung Ihrer Haut und Verbesserung der Ausstrahlung",
-        priceList: [{ time: 60, price: 89 }],
+        priceList: [{ time: 60, price: 99 }],
     },
     {
         title: "Anti Stress",
         description: "Beruhigung der gestressten und sensiblen Haut",
-        priceList: [{ time: 60, price: 89 }],
+        priceList: [{ time: 60, price: 99 }],
     },
     {
         title: "Schnell & Schön",
         description: "Klare, reine Hautstruktur",
-        priceList: [{ time: 45, price: 69 }],
+        priceList: [{ time: 45, price: 79 }],
     },
     {
-        title: "Jugend / Akne",
-        description: "Bis zum 18. Lebensjahr",
-        priceList: [{ time: 0, price: 49 }],
+        title: "Jugend",
+        description: "Tiefenreinigende Behandlung bis zum 18. Lebensjahr",
+        priceList: [{ time: 60, price: 59 }],
     },
     {
-        title: "Männersache",
+        title: "Rücken",
+        description: "Reinigung",
+        priceList: [{ time: 20, price: 49 }],
+    },
+    {
+        title: "Herren",
         description:
-            "Auftanken, Abschalten und mit neuer Energie und gepflegtem Aussehen durchstarten",
-        priceList: [{ time: 50, price: 69 }],
+            "Auftanken, Abschalten und mit neuer Energie und gepflegtem Aussehen starten",
+        priceList: [{ time: 50, price: 79 }],
     },
     {
         title: "Kleiner Augenblick",
-        description: "Wimpern und/oder Augenbrauen färben, zupfen, pflegen",
-        priceList: [{ time: 0, price: 39 }],
+        description: "Wimpern, Augenbrauen färben, zupfen und pflegen",
+        priceList: [{ time: 0, price: 49 }],
     },
     {
         title: "Wimpern-Lifting",
         description:
-            "geschwungene eigene Wimpern für einen atemberaubenden Augenaufschlag, färben und laminieren",
-        priceList: [{ time: 0, price: 79 }],
-    },
-    {
-        title: "Augenbrauen-Lifting",
-        description: "färben, zupfen und laminieren",
-        priceList: [{ time: 0, price: 59 }],
+            "geschwungene Wimpern für einen wunderschönen Augenaufschlag, färben und laminieren",
+        priceList: [{ time: 0, price: 89 }],
     },
     {
         title: "Maniküre",
         description: "Perfekt gepflegte Hände",
-        priceList: [{ time: 45, price: 45 }],
+        priceList: [{ time: 45, price: 55 }],
     },
     {
         title: "Pediküre",
         description: "Perfekt gepflegte Füße",
-        priceList: [{ time: 45, price: 49 }],
+        priceList: [{ time: 45, price: 59 }],
+    },
+    {
+        title: "Fußmassage",
+        description: "",
+        priceList: [{ time: 20, price: 49 }],
     },
 ];
 export const productExtras = [
-    { text: "Wimpern färben", price: 15 },
+    { text: "Wimpern färben", price: 18 },
     { text: "Augenbrauen färben", price: 12 },
-    { text: "Augenbrauen zupfen", price: 10, starting: true },
-    { text: "Enthaarung Oberlippe/Kinn", price: 12, starting: true },
-    { text: "Ampulle", price: 10 },
-    { text: "Bio Cellulose-Gesichtsmaske", price: 20 },
-    { text: "Augenmaske", price: 10 },
+    { text: "Augenbrauen zupfen", price: 12 },
+    { text: "Enthaarung Oberlippe/Kinn", price: 15, starting: true },
+    { text: "Ampulle", price: 12 },
+    { text: "Bio Cellulose-Gesichtsmaske", price: 22 },
+    { text: "Augenmaske", price: 12 },
     { text: "Lackieren", price: 15 },
+    { text: "Striplack", price: 20 },
 ];
 export const massages: TProductInfo[] = [
     {
-        title: "Ganzkörpermassage",
-        description: "",
-        priceList: [{ time: 50, price: 109 }],
-    },
-    {
         title: "Rückenmassage",
         description: "",
-        priceList: [{ time: 20, price: 69 }],
+        priceList: [{ time: 20, price: 79 }],
     },
     {
         title: "LA BEAUTÉ Luxusmassage",
         description: "Ganzkörpermassage und Bio Cellulose-Maske",
-        priceList: [{ time: 50, price: 129 }],
+        priceList: [{ time: 50, price: 139 }],
     },
 ];
 export const specialTreatment: TProductInfo[] = [
@@ -159,8 +157,8 @@ export const specialTreatment: TProductInfo[] = [
         description:
             "Hochfrequenter Ultraschall wirkt in der Tiefe der Haut effektiv gegen Falten, Tränensäcke, Couperose, Schwellungen und Elastizitätsverlust.",
         priceList: [
-            { time: 70, price: 139, text: "Inklusiv Bio Cellulose-Maske" },
-            { time: 70, price: 179, text: "Inklusiv Hals & Dekolleté" },
+            { time: 70, price: 149, text: "Inklusiv Bio Cellulose-Maske" },
+            { time: 70, price: 189, text: "Inklusiv Hals & Dekolleté" },
         ],
     },
     {
@@ -168,26 +166,26 @@ export const specialTreatment: TProductInfo[] = [
         description:
             "Tiefenbehandlung mit Langzeitwirkung sowie umfangreicher und gezielter Hautverbesserung auf 3 Ebenen.",
         priceList: [
-            { time: 70, price: 139, text: "Inklusiv Bio Cellulose-Maske" },
-            { time: 70, price: 179, text: "inklusiv Hals & Dekolleté" },
+            { time: 70, price: 149, text: "Inklusiv Bio Cellulose-Maske" },
+            { time: 70, price: 189, text: "inklusiv Hals & Dekolleté" },
         ],
     },
     {
         title: "Mikrodermabrasion und Ultraschall",
         description: "",
-        priceList: [{ time: 90, price: 199 }],
+        priceList: [{ time: 90, price: 209 }],
     },
     {
         title: "Pro Lift Mandelic Glow",
         description:
             "Strahlende Haut durch hochprozentige Mandelsäure. Faszinierende Glätte und ebenmäßige, gefestigte Haut",
-        priceList: [{ time: 90, price: 129 }],
+        priceList: [{ time: 90, price: 139 }],
     },
     {
         title: "Couperose / Rosazea Behandlung",
         description:
             "Kosmetisches Gefäßtraining für einen tief entspannten Teint. Stabilisierung und Entstauung der Gefäße bei Hautreizungen und Schwellungen.",
-        priceList: [{ time: 70, price: 119 }],
+        priceList: [{ time: 70, price: 129 }],
     },
 ];
 export const specialists = [
