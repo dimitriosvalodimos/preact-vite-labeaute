6edaf2dffb09802603434041284e5799e3695714
Dimitrios Valodimos <dimitriosvalodimos@gmail.com>
2025-07-27 19:59:34 +0200

    remove covid warning

--- src/components/ui/Warning/index.tsx
+++ src/components/ui/Warning/index.tsx
@@ -2,22 +2,12 @@ export const Warning = () => (
     <div
         style={{
             textAlign: "center",
-            backgroundColor: "#f5c6cb",
             borderRadius: "var(--radius)",
             padding: "1rem",
+            marginTop: "1rem",
             marginBottom: "1rem",
         }}
     >
-        <p>
-            Aufgrund der immer veränderten Situation bitten wir Sie die
-            aktuellen Corona Vorschriften im Geschäft unter
-        </p>
-        <p>040 813271</p>
-        <p> zu erfragen.</p>
-        <br />
-        <p>Wir wünschen Ihnen eine schöne Zeit.</p>
-        <p>Bleiben Sie gesund!</p>
-        <br />
         <p>Wir freuen uns auf Sie.</p>
         <p>Ihr La Beauté Team</p>
         <p>Kosmetik und Wellness</p>
